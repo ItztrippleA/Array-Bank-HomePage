02d9e4b4b9230cebea78c73e950cea19615c30b4
Abdullahi Adam <AbdullahiAdam.aa@gmail.com>
2021-03-05 00:13:20 +0100

    implemented scroll to and section effect for toggling on and off panels

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
 
 const openModal = function (e) {
   e.preventDefault();
@@ -28,4 +30,68 @@ document.addEventListener("keydown", function (e) {
   if (e.key === "Escape" && !modal.classList.contains("hidden")) {
     closeModal();
   }
+});
+
+btnScrollTo.addEventListener("click", function (e) {
+  const s1cords = section1.getBoundingClientRect();
+
+  // window.scrollTo(
+  //   s1cords.left + window.pageXOffset,
+  //   s1cords.top + window.pageYOffset
+  // );
+
+  //oldschool way
+  // window.scrollTo({
+  //   left: s1cords.left + window.pageXOffset,
+  //   top: s1cords.top + window.pageYOffset,
+  // });
+
+  section1.scrollIntoView({ behavior: `smooth` });
+});
+
+// document.querySelectorAll(`.nav__link`).forEach(function (el) {
+
+//   el.addEventListener(`click`, function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute(`href`);
+//     document.quuerySelector(id).scrollIntoView({ behavior: `smooth` });
+//   });
+// });
+
+////////////
+// 1.Add event listeners to comon parent
+// 2. Determine what element originated the event
+
+document.querySelector(`.nav__links`).addEventListener(`click`, function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains(`nav__link`)) {
+    const id = e.target.getAttribute(`href`);
+    document.querySelector(id).scrollIntoView({ behavior: `smooth` });
+  }
+});
+
+//Trabbed component
+
+const tabs = document.querySelectorAll(`.operations__tab`);
+const tabContainer = document.querySelector(`.operations__tab-container`);
+const tabContent = document.querySelectorAll(`.operations__content`);
+
+tabContainer.addEventListener(`click`, function (e) {
+  const clicked = e.target.closest(`.operations__tab`);
+
+  // Guard clause
+  if (!clicked) return;
+
+  //remove active classes
+  tabs.forEach((t) => t.classList.remove(`operations__tab--active`));
+  tabContent.forEach((c) => c.classList.remove(`operations__content--active`));
+
+  //activate tab
+  clicked.classList.add(`operations__tab--active`);
+
+  //content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add(`operations__content--active`);
 });
