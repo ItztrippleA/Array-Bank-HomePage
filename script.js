dcdc6fc3d5cdb0bd6cb3c9842620bc4056deb769
Abdullahi Adam <AbdullahiAdam.aa@gmail.com>
2021-03-06 02:33:45 +0100

    implemented slow reveal of UIs during scroll with the observer method

--- script.js
+++ script.js
@@ -9,6 +9,12 @@ const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 const btnScrollTo = document.querySelector(".btn--scroll-to");
 const section1 = document.querySelector("#section--1");
+const tabs = document.querySelectorAll(`.operations__tab`);
+const tabContainer = document.querySelector(`.operations__tab-container`);
+const tabContent = document.querySelectorAll(`.operations__content`);
+const nav = document.querySelector(`nav`);
+const header = document.querySelector(`.header`);
+const navHeight = nav.getBoundingClientRect().height;
 
 const openModal = function (e) {
   e.preventDefault();
@@ -73,10 +79,6 @@ document.querySelector(`.nav__links`).addEventListener(`click`, function (e) {
 
 //Trabbed component
 
-const tabs = document.querySelectorAll(`.operations__tab`);
-const tabContainer = document.querySelector(`.operations__tab-container`);
-const tabContent = document.querySelectorAll(`.operations__content`);
-
 tabContainer.addEventListener(`click`, function (e) {
   const clicked = e.target.closest(`.operations__tab`);
 
@@ -95,3 +97,94 @@ tabContainer.addEventListener(`click`, function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add(`operations__content--active`);
 });
+
+const handleHover = function (e) {
+  if (e.target.classList.contains(`nav__link`)) {
+    const link = e.target;
+    const siblings = link.closest(`.nav`).querySelectorAll(`.nav__link`);
+    const logo = link.closest(`.nav`).querySelector(`img`);
+
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+nav.addEventListener(`mouseover`, handleHover.bind(0.5));
+
+nav.addEventListener(`mouseout`, handleHover.bind(1));
+
+// const obsCallback = function (entries, observer) {
+//   entries.forEach((entry) => {
+//     console.log(entry);
+//   });
+// };
+
+// const obsOptions = {
+//   root: null,
+//   threshold: [0, 0.2],
+// };
+
+// const observer = new IntersectionObserver(obsCallback, obsOptions);
+// observer.observe(section1);
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) nav.classList.add(`sticky`);
+  else nav.classList.remove(`sticky`);
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `${navHeight}px`,
+});
+
+headerObserver.observe(header);
+
+//reaveal
+const allSections = document.querySelectorAll(`.section`);
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove(`section--hidden`);
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add(`section--hidden`);
+});
+
+//LazyLoading
+const imgTargets = document.querySelectorAll("img[data-src]");
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-img");
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: "200px",
+});
+
+imgTargets.forEach((img) => imgObserver.observe(img));
